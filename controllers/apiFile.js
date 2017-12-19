5f886011663b629b3b9cf4f658dd550854dfdc59
Vernon Reis <32598819+venom9122@users.noreply.github.com>
2017-12-18 17:15:51 -0800

    api controller added

---
+++ controllers/apiFile.js
@@ -0,0 +1,21 @@
+﻿const express = require('express');
+const request = require('request');
+const router = express.Router();
+
+const ApiKey = process.env.MONGODB_URI || require('../public/ApiKey');
+
+
+router.get('/', async (req, res) => {
+
+    test = request("http://api.brewerydb.com/v2/?key=" + ApiKey, (error, response, body) => {
+        myTest = JSON.parse(body);
+
+        
+
+        res.send(myTest);
+    });
+    // res.send(allUsers);
+});
+
+
+module.exports = router;
